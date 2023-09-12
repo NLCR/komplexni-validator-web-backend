2e54d1cf5116fcb5e8dc3ac04ca6b08b0d9ff1c3
Martin Řehánek <martin.rehanek@gmail.com>
2023-09-12 17:04:27 +0200

    cleanup

--- quotas-service/src/main/java/npk/pspValidator/web/backend/quotas_service/db/QuotasDatabase.java
+++
@@ -1,9 +0,0 @@
-package npk.pspValidator.web.backend.quotas_service.db;
-
-import java.util.Map;
-
-public interface QuotasDatabase {
-    Map<String, Integer> getQuotas();
-
-    void setQuota(String key, Integer value);
-}
